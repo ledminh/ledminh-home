b7fdd4d7ed0945cee1f7d20dd9dc049a83fcfe54
ledminh <duyminh.le310@gmail.com>
2017-03-28 22:48:33 -0700

    Trim down Now and Then

--- app/containers/ProfilePicture/frame.js
+++ app/containers/ProfilePicture/frame.js
@@ -1,22 +1,23 @@
 import styled from 'styled-components';
 
-const left = `
-  overflow: hidden;
-  &:hover {
-    box-shadow: 0px 0px 30px;
-  }
-`;
-
-const top = `
-  background-color: #e2e2e2;  
-  text-align: center;
-`;
 
 const Frame = styled.div`
   position: relative;
   width: 100%;
+  overflow: hidden;
 
-  ${(props) => (props.position === "left")? left : top }
+  &:hover {
+    box-shadow: 0px 0px 30px;
+  }
+
+  @media screen and (max-width: 700px){
+    background-color: #e2e2e2;
+    text-align: center;
+
+    &:hover {
+      box-shadow: none;
+    }
+  }
 
 `;
 
